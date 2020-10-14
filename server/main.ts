05ecee9f1efd1f64ca967eca9075e861201abf8b
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-14 16:38:06 +0530

    configured deno oak server

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,25 @@
-import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { green, yellow } from "https://deno.land/std@0.53.0/fmt/colors.ts";
 
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+const app = new Application();
+const port: number = 8080;
 
-for await (const request of server) {
-  let bodyContent = "Your user-agent is:\n\n";
-  bodyContent += request.headers.get("user-agent") || "Unknown";
+const router = new Router();
 
-  request.respond({ status: 200, body: bodyContent });
-}
+router.get("/", ({ response }: { response: any }) => {
+  console.log("Incoming Request");
+  response.body = {
+    message: "Helloworld",
+  };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  console.log(`${yellow("Listening on:")} ${green(url)}`);
+});
+
+await app.listen({ port });
