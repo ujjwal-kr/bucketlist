ece9dc303c920a46a57f068a3ea49fb040d86467
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-22 19:15:32 +0530

    opacity settings

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -14,11 +14,11 @@ class Home extends React.Component<Props, State> {
                     <Brand className="branding">Bucketlist</Brand>
                     <br/>
                     <BrandText className="quicktext">
-                        Welcome, this is in testing stage so you wont be able to register unless you are a tester.
+                        Welcome, this is in testing stage so you wont be able to register unless you are a tester. You can view the source code here.
                     </BrandText>
                     <br/>
                     <Button variant="contained" color="secondary" size="large">LogIn</Button><br/><br/>
-                    <Button variant="contained" color="secondary" size="large">SignUp</Button> 
+                    <Button variant="contained" color="secondary" size="large">SignUp</Button>
                 </Wrapper>
             </Overlay>
         </Main>;
