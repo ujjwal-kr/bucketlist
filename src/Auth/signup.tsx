deb2b2fe71f5da5d57367a5cea4a5a0766fdab2b
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-25 17:15:31 +0530

    added signup component

--- src/Auth/signup.tsx
+++ src/Auth/signup.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
+import { Main, Overlay } from "../Components/app";
+import { Heading } from "../Components/auth";
+import { Button, TextField } from "@material-ui/core";
+import { useFormik } from "formik";
 
-interface Props { }
-interface State { }
 
-class Signup extends React.Component<State, Props> {
-    render() {
-        return (
-            <div>
-                signup
-            </div>
-        )
-    }
+const Signup = () => {
+
+    const formik = useFormik({ 
+        initialValues: {
+            username: "",
+            password: "",
+            code: ""
+        },
+        onSubmit: values => {
+            console.log(values)
+        }
+     })
+
+    return (
+        <Main>
+            <Overlay>
+                <Heading className="stylish animate__animated animate__zoomIn animate__faster">SignUp</Heading>
+                <br />
+                <form onSubmit={formik.handleSubmit} style={{ textAlign: 'center' }} className="animate__animated animate__zoomIn animate__faster">
+                    <TextField
+                        onChange={formik.handleChange}
+                        value={formik.values.username}
+                        name="username"
+                        label="Username"
+                        variant="filled"
+                        style={{ background: 'white' }}
+                        color="secondary"
+                    />
+                    <br /> <br />
+                    <TextField
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        name="password"
+                        label="Password"
+                        variant="filled"
+                        style={{ background: 'white' }}
+                        color="secondary"
+                        type="password"
+                    /> <br /><br />
+                    <TextField
+                        onChange={formik.handleChange}
+                        value={formik.values.code}
+                        name="code"
+                        label="Code For Taks"
+                        variant="filled"
+                        style={{ background: 'white' }}
+                        color="secondary"
+                    /> <br /><br />
+                    <Button size="large" variant="contained" color="secondary" type="submit">Submit</Button>
+                </form>
+            </Overlay>
+        </Main>
+    )
 }
 
 export default Signup;
