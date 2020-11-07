aa58b47be051793ffe6fc8c1f1290033e4c5848d
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-07 14:07:20 +0530

    created form for code

--- src/weekly/code.tsx
+++ src/weekly/code.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { Main, Overlay } from '../Components/app';
+import { useFormik } from 'formik';
+import { Heading } from '../Components/auth';
 
-const TaskCode = function() {
-    return <h1>Task Code</h1>
+import { TextField, Button } from '@material-ui/core';
+
+const TaskCode: FunctionComponent = function() {
+
+    const formik = useFormik({
+        initialValues: {
+            code: ''
+        },
+        onSubmit: values => {
+            const code = btoa(values.code)
+            console.log(code)
+        }
+    })
+
+    return <Main>
+        <Overlay>
+        <Heading className="stylish animate__animated animate__zoomIn animate__faster">Task Code</Heading>
+                <br />
+                <form onSubmit={formik.handleSubmit} style={{ textAlign: 'center' }} className="animate__animated animate__zoomIn animate__faster">
+                    <TextField
+                        onChange={formik.handleChange}
+                        value={formik.values.code}
+                        name="code"
+                        label="Task Code"
+                        variant="filled"
+                        style={{ background: 'white' }}
+                        color="secondary"
+                    />
+                    <br /> <br />
+                    <Button size="large" variant="contained" color="secondary" type="submit">Submit</Button>
+                </form>
+        </Overlay>
+    </Main>
 }
 
 export default TaskCode;
