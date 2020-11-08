789677cdecd30fc611a1d0abef706d93371adf9e
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-08 14:02:59 +0530

    postlist login

--- src/services/list.tsx
+++ src/services/list.tsx
@@ -6,6 +6,12 @@ export const ListService = {
         return axios.get(ENDPOINT+"lists/"+id)
     },
 
+    postItem: async function(token: string, text: string, description: string) {
+        return axios.post(ENDPOINT+"lists", {text, description}, {
+            headers: {'authorization': token}
+        })
+    },
+
     editItem: async function(id: string, item: any, token: string) {
         return axios.patch(ENDPOINT+"lists/"+id, item, {
             headers: {'authorization': token}
