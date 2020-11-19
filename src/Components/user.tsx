8a454fe663aff603de106c8896520feaad7785a7
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-19 23:29:20 +0530

    user basic style

---
+++ src/Components/user.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Username = styled.h1`
+    text-align: center;
+`
+
+export const Lists = styled.ul`
+    padding: 2%;
+`
+
+export const List = styled.li`
+    background: pink;
+`
