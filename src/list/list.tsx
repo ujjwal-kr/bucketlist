e03d135d4d200739d70eddf7a1a3d03a0bddfdce
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-20 15:39:32 +0530

    delete func

--- src/list/list.tsx
+++ src/list/list.tsx
@@ -13,31 +13,47 @@ interface Props {
 interface State {
     loading: boolean;
     list: List;
+    showDelete: boolean;
 }
 
 export interface List {
     text?: string;
     description?: string;
     id?: string;
+    userid?: string;
 }
 
 class ListComponent extends React.Component<Props, State> {
     state: State = {
         loading: true,
-        list: {}
+        list: {},
+        showDelete: false,
     }
 
     componentDidMount() {
+        const id = localStorage.getItem("name")
         ListService.getLItem(this.props.match.params.id).then(res => {
             this.setState({list: res.data, loading: false})
+            if(this.state.list.userid === id) {this.setState({showDelete: true})}
         }).catch(e => {
             alert("Something is not right")
             this.props.history.push('/users')
         })
     }
 
+    delete(e: any) {
+        e.preventDefault();
+        const token = localStorage.getItem("token")!
+        ListService.deleteItem(this.props.match.params.id, token).then(res => {
+            alert("Deleted")
+            this.props.history.push('/users'+this.state.list.userid)
+        }).catch(e => {
+            alert("Something is not right")
+        })
+    }
+
     render() {
-        return <h1>MEOW</h1>
+        return <h1>{this.state.list.text}</h1>
     }
 }
 
