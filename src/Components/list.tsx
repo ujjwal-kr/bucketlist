ac20e8a409a21e8337b127ae982734f2395544c1
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-20 16:12:45 +0530

    markup list

---
+++ src/Components/list.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Username = styled.h1`
+    text-align: center;
+`
+
+export const Text = styled.h3`
+    text-align: center;
+` 
+
+export const Description = styled.p`
+    text-align: center;
+`
--- src/list/list.tsx
+++ src/list/list.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ListService } from '../services/list';
+import {Username, Description, Text} from '../Components/list';
+import { Button } from '@material-ui/core';
 
 interface Props {
     history: any;
@@ -24,6 +26,10 @@ export interface List {
 }
 
 class ListComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.delete = this.delete.bind(this)
+    }
     state: State = {
         loading: true,
         list: {},
@@ -53,7 +59,14 @@ class ListComponent extends React.Component<Props, State> {
     }
 
     render() {
-        return <h1>{this.state.list.text}</h1>
+        return (
+            <main style={{padding: 2+'%'}}>
+                <Username className="quicktext">{this.state.list.userid}</Username>
+                <Text className="quicktext">{this.state.list.text}</Text>
+                <Description className="fira" >{this.state.list.description}</Description>
+                {this.state.showDelete ? <Button onClick={this.delete} color="secondary" variant="outlined" style={{width: 100+'%'}}>Delete</Button> :null}
+            </main>
+        )
     }
 }
 
