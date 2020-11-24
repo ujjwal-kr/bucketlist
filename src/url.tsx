91f22a3fd465280cff1bd3222356d5350f6fa999
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-24 20:49:45 +0530

    url

--- src/url.tsx
+++ src/url.tsx
@@ -1,2 +1,2 @@
-// export const ENDPOINT = "https://go-bucketlist-server.herokuapp.com/";
-export const ENDPOINT = "http://localhost:8080/"
+export const ENDPOINT = "https://go-bucketlist-server.herokuapp.com/";
+// export const ENDPOINT = "http://localhost:8080/"
