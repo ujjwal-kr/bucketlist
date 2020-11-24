fdc20b5d5e8c6ca6ac39b7db7db04cfb21f638d8
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-24 19:01:36 +0530

    task logic

--- src/weekly/tasks.tsx
+++ src/weekly/tasks.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { UserService } from '../services/user';
+import { User } from '../users/users';
+import Loader from '../loader/loader';
 
 interface Props {
     history: any;
@@ -11,25 +13,47 @@ interface Props {
     }
 }
 
-interface State {}
+interface State {
+    user: User;
+    tasks: Task[];
+    loading: boolean;
+}
+
+interface Task {
+    text?: string;
+    date?: any;
+    userid?: string;
+}
 
 class Tasks extends React.Component<Props, State>{
+
+    state: State = {
+        user: {username: '', id: ''},
+        tasks: [],
+        loading: true,
+    }
 
     componentDidMount() {
         const token = localStorage.getItem("token")!
         const code = atob(this.props.match.params.code)!
         const id = this.props.match.params.id!
         if (!token) {
-            this.props.history.push("/")
+            alert('something went wrong')
+            this.props.history.push("/users/" +localStorage.getItem("name"))
         }
         UserService.getTasks(token, id, code).then(res => {
-            console.log(res.data)
+            this.setState({user: res.data.user, tasks: res.data.tasks, loading: false})
         }).catch(e => {
-            this.props.history.push("/")
+            alert('something went wrong')
+            this.props.history.push("/users/" +localStorage.getItem("name"))
         })
     }
 
     render() {
+        if(this.state.loading) {
+            return (<div><br/><Loader /></div>)
+        }
+        
         return (
             <div>
                 Tasks
