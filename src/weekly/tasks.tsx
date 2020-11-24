7e250f9ee5a692b620a5d9d38e1949a6a3bc6281
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-24 20:20:43 +0530

    done with delete

--- src/weekly/tasks.tsx
+++ src/weekly/tasks.tsx
@@ -5,6 +5,7 @@ import Loader from '../loader/loader';
 import { Username, Text } from '../Components/list';
 import { Button, Paper } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { TasksService } from '../services/tasks';
 
 interface Props {
     history: any;
@@ -20,9 +21,11 @@ interface State {
     user: User;
     tasks: Task[];
     loading: boolean;
+    showDelete?: boolean;
 }
 
 interface Task {
+    id?: string;
     text?: string;
     date?: any;
     userid?: string;
@@ -30,13 +33,20 @@ interface Task {
 
 class Tasks extends React.Component<Props, State>{
 
+    constructor(props: Props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+
     state: State = {
         user: {username: '', id: ''},
         tasks: [],
         loading: true,
+        showDelete: false,
     }
 
     componentDidMount() {
+        this.setState({loading: true})
         const token = localStorage.getItem("token")!
         const code = atob(this.props.match.params.code)!
         const id = this.props.match.params.id!
@@ -47,8 +57,18 @@ class Tasks extends React.Component<Props, State>{
         UserService.getTasks(token, id, code).then(res => {
             this.setState({user: res.data.user, tasks: res.data.tasks, loading: false})
         }).catch(e => {
-            alert('something went wrong')
+            console.log(e)
+            alert('something went wrong!')
             this.props.history.push("/users/" +localStorage.getItem("name"))
+        })
+    }
+
+    delete(id: any) {
+        const token = localStorage.getItem("token")!;
+        TasksService.deleteItem(token, id).then(res => {
+            return this.componentDidMount()
+        }).catch(e => {
+            alert("Cannot Delete")
         })
     }
 
@@ -58,7 +78,7 @@ class Tasks extends React.Component<Props, State>{
         }
 
         return (
-            <div>
+            <div className="animate__animated animate__zoomIn animate__faster" style={{padding: '3%'}}>
                 <Username className="quicktext">{this.state.user.username}</Username>
                 <Text className="fira">Tasks</Text>
                 <div style={{textAlign: 'center'}}>
@@ -67,7 +87,9 @@ class Tasks extends React.Component<Props, State>{
                     </Link>
                 </div>
 
-        {this.state.tasks.map((task, i) => <div><Paper elevation={2} className="fira" style={{padding: 2.1+'%', fontSize: 1.1+'em'}}>{task.text}</Paper><br/></div>)}
+        {this.state.tasks.map((task, i) => <div><Paper elevation={2} className="fira" style={{padding:2.1+'%', fontSize: 1.1+'em'}}><p>{task.text}</p></Paper>
+            <Button color="secondary" onClick={() => this.delete(task.id)}>Delete</Button>
+        <br/></div>)}
             </div>
         )
     }
